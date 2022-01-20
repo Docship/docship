324b6acc32e1804a0bb55bf0bf2fbb74aabc057e
NirmalSankalana <nirmalsankalana71@gmail.com>
2022-01-20 22:09:48 +0530

    Change send button of chat

--- app/views/patient/messages.php
+++ app/views/patient/messages.php
@@ -14,8 +14,8 @@
             <div class="overflow-scroll px-0 py-2 imessage d-flex chat-box">
                 
             </div>
-            <div class="row send m-0 px-0 pt-1 align-items-center border-top">
-            <form action="#" class="typing-area" autocomplete="off">
+
+            <form action="#" class="typing-area d-flex" autocomplete="off">
                     <div class="col-10 col-md-11">
                         <input type="text" class="form-control input-field" name="message" placeholder="Typing a message here....">  
                     </div>
@@ -24,7 +24,7 @@
                     </div>
             </form>
             <script src="<?php echo URLROOT; ?>/js/chat-sent-patient.js"></script>        
-            </div>            
+          
         </div>
     </div>
 
